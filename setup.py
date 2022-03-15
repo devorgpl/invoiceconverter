8843040e3cb01641ed12e6b66126b6f3d4c2e9b6
Cyprian <cyprian@sniegota.com>
2022-03-15 14:25:09 +0100

    Fix unknown chars

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='invoiceconverterlib',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='Invoice converter library',
     author='Cyprian Śniegota',
     license='Apache Licence 2.0',
